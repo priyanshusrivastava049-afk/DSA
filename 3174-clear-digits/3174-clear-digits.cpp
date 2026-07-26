d1d848a97064fe36ce2eb4a9cb933689ffad5118
priyanshusrivastava049-afk <priyanshusrivastava049@gmail.com>
2026-07-27 01:13:49 +0530

    Time: 0 ms (100%), Space: 9.2 MB (11.07%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(!st.empty() && isdigit(s[i])){
+                st.pop();
+            }
+            else
+            st.push(s[i]);
+        }
+        string res;
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin() , res.end());
+        return res;
+    }
+};
