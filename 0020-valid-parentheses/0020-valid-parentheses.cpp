2a247f49f860a6c22c7945d248a1a57afbfa3ff0
priyanshusrivastava049-afk <priyanshusrivastava049@gmail.com>
2026-07-30 12:07:22 +0530

    Time: 0 ms (100%), Space: 9 MB (38.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                if (st.top() == '(' && ch == ')' ||
+                    st.top() == '{' && ch == '}' ||
+                    st.top() == '[' && ch == ']') {
+                    st.pop();
+                }
+                else {
+                return false;}
+            }
+        }
+        if (st.empty()) {
+            return true;
+        }
+        return false;
+    }
+};
